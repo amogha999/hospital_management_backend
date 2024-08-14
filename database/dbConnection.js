fc481649b9273755e9546a6216e50a28f124bf28
amogha999 <mangaloreamogha@gmail.com>
2024-08-14 18:48:38 +0530

    db

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection=()=>{
     mongoose.connect(process.env.MONGO_URI,{
-        dbName:" Hospital_management",
+        dbName:'Hospital_management',
     }).then(()=>{
         console.log("DB Connected")
     }).catch(err=>{
